ecf0499e23374cb4a2a4e0b1d0b0e2905f97ffce
KR3915 <ondrej.brdek@gmail.com>
2024-12-19 09:40:07 +0100

    piskvorky.py

--- piskvorky.py
+++ piskvorky.py
@@ -9,124 +9,80 @@ plocha = ["-","-","-",
           "-","-","-",
           "-","-","-"]
 P1 = plocha
-IsX = True
+
 
 
 
 
 print ("Welcome to Tic Tac Toe \n========================================\n GAME RULES:\n Each player can place one mark (or stone)\n per turn on the 3x3 grid. The WINNER is\n who succeeds in placing three of their\n marks in a:\n * horizontal,\n * vertical or\n * diagonal row")
-print ("+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+\n")
+print ("+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n")
 while end == False:
-  if IsX == True:
-    IsX = False
-    print("========================================")
-    print ("Hráč X zvolte pozici")
-    print("========================================")
-    pozice = int(input())
-    indx = pozice - 1
-    if plocha[indx] != "-":
-      while plocha[indx] != "-":
-        print("toto pole je obsazeno, zvolte prazdnou pozici: ")
-        pozice = int(input())
-        indx = pozice - 1
-    else:
-      plocha[indx] = "X"
-      print ("+---+---+---+\n | "+ plocha[0] +" | " + plocha[1] + " | " + plocha[2] + " |\n+---+---+---+\n| " + plocha[3] + " | " + plocha[4] + " | " + plocha[5] + " |\n+---+---+---+\n| " + plocha[6] +" | " + plocha[7] + " | " + plocha[8] + " |\n+---+---+---+\n")
-      if plocha[0] == "X":
-        if plocha[1] == "X":
-          if plocha[2] == "X":
-            end = True
-            WIN = "X"
-        elif plocha[3] == "X":
-          if plocha[6] == "X":
-            end = True
-            WIN = "X"
-        elif plocha[4] == "X":
-          if plocha[8] == "X":
-            end = True
-            WIN = "X"
-      elif plocha[3] == "X":
-        if plocha[4] == "X":
-          if plocha[5] == "X":
-            end = True
-            WIN = "X"
-      elif plocha[6] == "X":
-        if plocha[7] == "X":
-          if plocha[8] == "X":
-            end = True
-            WIN = "X"
-      elif plocha[1] == "X":
-        if plocha[4] == "X":
-          if plocha[7] == "X":
-            end = True
-            WIN = "X"
-      elif plocha[2] == "X":
-        if plocha[5] == "X":
-          if plocha[8] == "X":
-            end = True
-            WIN = "X"
-        elif plocha[4] == "X":
-          if plocha[6] == "X":
-            end = True
-            WIN = "X"
-#HRAC O
-    print(IsX)
-  elif IsX == False:
-    IsX = True
-    print("========================================")
-    print ("Hráč O zvolte pozici")
-    print("========================================")
-    pozice = int(input())
-    indx = pozice - 1
-    if plocha[indx] != "-":
-      while plocha[indx] != "-":
-        print("toto pole je obsazeno, zvolte prazdnou pozici: ")
-        pozice = int(input())
-        indx = pozice - 1
-    else:
-      plocha[indx] = "O"
-      print ("+---+---+---+\n | "+ plocha[0] +" | " + plocha[1] + " | " + plocha[2] + " |\n+---+---+---+\n| " + plocha[3] + " | " + plocha[4] + " | " + plocha[5] + " |\n+---+---+---+\n| " + plocha[6] +" | " + plocha[7] + " | " + plocha[8] + " |\n+---+---+---+\n")
-      if plocha[0] == "O":
-        if plocha[1] == "O":
-          if plocha[2] == "O":
-            end = True
-            WIN = "O"
-        elif plocha[3] == "O":
-          if plocha[6] == "O":
-              end = True
-              WIN = "O"
-        elif plocha[4] == "O":
-          if plocha[8] == "O":
-            end = True
-            WIN = "O"
-      elif plocha[3] == "O":
-       if plocha[4] == "O":
-        if plocha[5] == "O":
-           end = True
-           WIN = "O"
-      elif plocha[6] == "O":
-        if plocha[7] == "O":
-         if plocha[8] == "O":
-           end = True
-           WIN = "O"
-      elif plocha[1] == "O":
-       if plocha[4] == "O":
-          if plocha[7] == "O":
-           end = True
-           WIN = "O"
-      elif plocha[2] == "O":
-        if plocha[5] == "O":
-          if plocha[8] == "O":
-            end = True
-            WIN = "O"
-        elif plocha[4] == "O":
-          if plocha[6] == "O":
-            end = True
-            WIN = "O"
+  if Player == "O":
+    Player = "X"
+  else:
+    Player = "O"
 
+  print("========================================")
+  print ("Hráč " + Player + " zvolte pozici")
+  print("========================================")
+  pozice = int(input())
+  indx = pozice - 1
+  if 0>int(pozice):
+    while 0>int(pozice):
+      print("zvolte číslo mezi 1-9: ")
+      pozice = int(input())
+
+  elif 9<int(pozice):
+    while 9<int(pozice):
+      print("zvolte číslo mezi 1-9: ")
+      pozice = int(input())
+  elif plocha[indx] != "-":
+    while plocha[indx] != "-":
+      print("toto pole je obsazeno, zvolte prazdnou pozici: ")
+      pozice = int(input())
+      indx = pozice - 1
+  else:
+     plocha[indx] = Player
+     print ("+---+---+---+\n | "+ plocha[6] +" | " + plocha[7] + " | " + plocha[8] + " |\n+---+---+---+\n| " + plocha[3] + " | " + plocha[4] + " | " + plocha[5] + " |\n+---+---+---+\n| " + plocha[0] +" | " + plocha[1] + " | " + plocha[2] + " |\n+---+---+---+\n")
+     if plocha[0] == Player:
+        if plocha[1] == Player:
+          if plocha[2] == Player:
+             end = True
+             WIN = Player
+        elif plocha[3] == Player:
+          if plocha[6] == Player:
+            end = True
+            WIN = Player
+        elif plocha[4] == Player:
+          if plocha[8] == Player:
+            end = True
+            WIN = Player
+     elif plocha[3] == Player:
+        if plocha[4] == Player:
+           if plocha[5] == Player:
+             end = True
+             WIN = Player
+     elif plocha[6] == Player:
+        if plocha[7] == Player:
+          if plocha[8] == Player:
+            end = True
+            WIN = Player
+     elif plocha[1] == Player:
+       if plocha[4] == Player:
+         if plocha[7] == Player:
+            end = True
+            WIN = Player
+     elif plocha[2] == Player:
+         if plocha[5] == Player:
+          if plocha[8] == Player:
+            end = True
+            WIN = Player
+         elif plocha[4] == Player:
+           if plocha[6] == Player:
+            end = True
+            WIN = Player
 else:
   print ("hrac "+ WIN + " vyhral")
-
 
 
 
